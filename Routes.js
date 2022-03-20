6a2e7412b058a9edb63cf56b702a8000cd60955c
Joao7747 <joaoguilherme.sbc@gmail.com>
2022-03-20 14:04:23 -0300

    apis commit

--- Routes.js
+++ Routes.js
@@ -1,3 +1,7 @@
 const loginRoute = require("./Routes/LoginRoute");
+const contaRoute = require("./Routes/ContaRoute");
+const carteiraRoute = require("./Routes/CarteiraRoute");
 
 module.exports = loginRoute
+module.exports = contaRoute
+module.exports = carteiraRoute
