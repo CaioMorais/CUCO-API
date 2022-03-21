23ec4ea068e7135904de7eb6fae2f3eb2361cdcf
Joao7747 <joaoguilherme.sbc@gmail.com>
2022-03-21 16:02:51 -0300

    Swagger edit

--- Routes/GerenciamentoRoute.js
+++ Routes/GerenciamentoRoute.js
@@ -21,24 +21,6 @@ const GerenciamentoController = require("../Controllers/GerenciamentoController"
  */
 
 
-/**
- * @swagger
- * tags:
- *  name: GerenciamentoRoute
- *  description: API's de Gerenciamento
- * /api/Gerenciamento/HistoricoRetiradas:
- *  get:
- *      tags: [Gerenciamento]
- *      parameters:
- *          - name: page_number
- *            default: 1
- *            in: body
- *            schema:
- *              type: string
- *      responses:
- *          default:
- *              description: This is the default response for it
- */
 Gerenciamentorouter.get('Gerenciamento/HistoricoDoacoes', GerenciamentoController.HistoricoDoacoes)
 Gerenciamentorouter.get('Gerenciamento/ListaOngs', GerenciamentoController.ListaOngs)
 Gerenciamentorouter.get('Gerenciamento/Solicitacoes', GerenciamentoController.Solicitacoes)
