923d7729f15efb5bdf181d9f732adbd26ced96f8
junior freitas <jr13freitas@hotmail.com>
2022-05-03 20:33:16 -0300

    Limpeza nas Apis de Doação

--- Services/DoacaoService.js
+++ Services/DoacaoService.js
@@ -1,5 +1,8 @@
-function gerarQRCodeLinkDoacao(){
 
+function gerarQRCodeLinkDoacao(){
+    // acessar o banco paara retornar o link real com os dados do estabelecimento ja prenchidos
+    var link = "www.estabelecimentox/paginadoacao"
+    return link
 }
 
 function efetuaDoacao(){
@@ -22,12 +25,16 @@ function gerarTokenIndentificacaoRetiradaDoacoes(){
 
 //Estabelecimento
 function gerarTokenIndentificacaoEntregaDoacoes(){
-
+    var result = '';
+    for (var i = 80; i > 0; --i) {
+        result += (Math.floor(Math.random()*256)).toString(16);
+    }
+    return result;
 }
 
 //Ong Estabelecimento
 function validacaoTokens(){
-   
+    
 }
 
 module.exports = {
