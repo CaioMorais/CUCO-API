55290b2c70d9a38b51a70fa4aa6a6e356dbfe4f1
junior freitas <jr13freitas@hotmail.com>
2022-05-04 22:36:41 -0300

    Correção de erro

--- Controllers/v1/LoginController.js
+++ Controllers/v1/LoginController.js
@@ -1,6 +1,6 @@
 let Result = require("../../Domain/Entities/Result.js")
 const {realizarLogin} = require("../../Services/v1/LoginService")
- n
+ 
 exports.autenticar = (req, res, next) =>{
     result = new Result({"token": "caioeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImF5bGFuQGJvc2Nhcmluby5jb20iLCJwYXNzd29yZCI6InlhMGdzcWh5NHd6dnV2YjQifQ.yN_8-Mge9mFgsnYHnPEh_ZzNP7YKvSbQ3Alug9HMCsM", "type": "Bearer"}, true, "Logado com sucesso")
     res.status(200).send(result)
