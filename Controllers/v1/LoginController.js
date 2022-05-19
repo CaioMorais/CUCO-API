89e410e3c9d1659fe2e8a5d52969c63f0f7118e2
junior freitas <jr13freitas@hotmail.com>
2022-05-18 22:34:08 -0300

    Hash na senha e post na autenticação

--- Controllers/v1/LoginController.js
+++ Controllers/v1/LoginController.js
@@ -1,14 +1,10 @@
-const express = require("express");
-let Result = require("../../Domain/Entities/Result.js")
 const {realizarLogin} = require("../../Services/v1/LoginService")
  
 exports.autenticar = async (req, res) =>{
-    const email =  req.query.email;
-    const senha =  req.query.senha;
-    const resposta = await realizarLogin(email, senha);
-    if (resposta == 'User not found') return res.status(401).send(resposta);
-    const result = resposta
-    res.status(200).send(result)
+    const email =  req.body.email;
+    const senha =  req.body.senha;
+    const result = await realizarLogin(email, senha);
+    res.status(result.status).send(result)
 }
 
 
