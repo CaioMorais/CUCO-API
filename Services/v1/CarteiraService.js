66cbe56a15883263628d723065d18515327c8e3d
CaioMorais <caiomorais99@outlook.com>
2023-04-04 20:09:43 -0300

    listagem de carteiras da ONG

--- Services/v1/CarteiraService.js
+++ Services/v1/CarteiraService.js
@@ -41,7 +41,7 @@ async function listagemCarteiraIDRestaurante(id) {
             "valorPrato": carteira.valorPrato,
             "descPratoDoado": carteira.descPratoDoado,
             "entregasPendentes": carteira.entregasPendentes,
-            "nomeOng" : ong.nomeEstabelecimento
+            "nomeOng": ong.nomeEstabelecimento
          }
 
          var result = new Result(carteiraResult, true, "Carteira restaurante", 200);
@@ -57,31 +57,34 @@ async function listagemCarteiraIDRestaurante(id) {
    }
 }
 
-async function listagemCarteiraIDOng(id) {
+const listagemCarteiraIDOng = async function(id) {
    try {
-      var result;
+      var result = new Result();
+      result.content = [];
       var carteira = await carteiraSchema
-         .findOne({ idOng: id, status: "true" });
+         .find({ idOng: id, status: "true" });
 
       if (carteira) {
-
-         var restaurante = await estabelecimentoSchema.findById(carteira.idRestaurante);
-
-         var carteiraResult = {
-            "metaFinal": carteira.metaFinal,
-            "valorAtual": carteira.valorAtual,
-            "idRestaurante": carteira.idRestaurante,
-            "idOng": carteira.idOng,
-            "valorPrato": carteira.valorPrato,
-            "descPratoDoado": carteira.descPratoDoado,
-            "entregasPendentes": carteira.entregasPendentes,
-            "nomeRestaurante" : restaurante.nomeEstabelecimento
+         for (let index = 0; index < carteira.length; index++) {
+            var restaurante = await retornaNomeEstalecimento(carteira[index].idRestaurante);
+            var carteiraResult = {
+               "metaFinal": carteira[index].metaFinal,
+               "valorAtual": carteira[index].valorAtual,
+               "idRestaurante": carteira[index].idRestaurante,
+               "idOng": carteira[index].idOng,
+               "valorPrato": carteira[index].valorPrato,
+               "descPratoDoado": carteira[index].descPratoDoado,
+               "entregasPendentes": carteira[index].entregasPendentes,
+               "nomeRestaurante": restaurante
+            }
+            result.content.push(carteiraResult);
          }
-
-         var result = new Result(carteiraResult, true, "lista de Carteiras Ong", 200);
+         result.success = true;
+         result.status = 200
+         result.messages = "" 
       }
       else {
-         result = new Result(carteira, false, "Nenhuma Carteira encontrada", 200);
+         result = new Result(carteira, false, "Nenhuma Carteira encontrada", 204);
       }
       return result;
 
@@ -348,6 +351,11 @@ const insereValorCarteiraSemMetaAtingida = async (carteira, valorDoado) => {
    } catch (error) {
       return retorno = null;
    }
+}
+
+const retornaNomeEstalecimento = async function (idEstabelecimento) {
+   var result = await estabelecimentoSchema.findById(idEstabelecimento);
+   return result.nomeEstabelecimento;
 }
 
 module.exports = {
